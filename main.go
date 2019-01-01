a80c5dde022a095a8d825ba241142659def3dee2
Andrew Merenbach <andrew@merenbach.com>
2019-01-01 11:44:50 -0800

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
+// Copyright 2018 Andrew Merenbach
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// TODO: support multiple audio formats and don't tightly couple to MP3
+// TODO: unique code id for sounds so we can put into "id" attribute instead of data attribute? or use for data anyway?
+// TOOD: how are we cleaning up sound names? can we read ID3 tags?? but what of wavs? trim extension?
+
 package main
 
 import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"path"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 )
@@ -36,18 +56,25 @@ func (e Track) newerThan(s int64) bool {
 
 // A Playlist contains an ordered list of tracks to play.
 type Playlist struct {
-	Timeout    time.Duration     `json:"timeout"`
-	Library    map[string]string `json:"library"`
+	Timeout    time.Duration `json:"timeout"`
+	Library    []string      `json:"library"`
+	inLibrary  map[string]struct{}
 	tracks     []Track
 	tracksLock sync.RWMutex
 }
 
 // NewPlaylist creates a new Playlist with the given timeout.
-func NewPlaylist(library map[string]string, s int64) *Playlist {
+func NewPlaylist(ss []string, s int64) *Playlist {
+	inLibrary := make(map[string]struct{})
+	for _, s := range ss {
+		inLibrary[s] = struct{}{}
+	}
+
 	return &Playlist{
-		tracks:  []Track{},
-		Timeout: time.Duration(s),
-		Library: library,
+		tracks:    []Track{},
+		Timeout:   time.Duration(s),
+		Library:   ss,
+		inLibrary: inLibrary,
 	}
 }
 
@@ -71,7 +98,7 @@ func (p *Playlist) Prune() {
 
 // Append a new Track to the end of a Playlist.
 func (p *Playlist) Append(t Track) error {
-	if _, ok := p.Library[t.Resource]; !ok {
+	if _, ok := p.inLibrary[t.Resource]; !ok {
 		return errors.New("invalid track")
 	}
 
@@ -111,18 +138,14 @@ func (eq *EventQueue) Filtered() []Event {
 }*/
 
 // ServePlaylist runs an HTTP server with a playlist queue.
-func ServePlaylist(rr []string) {
-	// Sort resources alphabetically.
-	sort.Slice(rr, func(i, j int) bool {
-		return rr[i] < rr[j]
-	})
+func ServePlaylist(ss []string) {
 
-	library := make(map[string]string)
-	for _, r := range rr {
-		library[r] = "hello"
-	}
+	// library := make(map[string]string)
+	// for _, r := range rr {
+	// 	library[r] = "hello"
+	// }
 
-	playlist := NewPlaylist(library, defaultExpireSeconds)
+	playlist := NewPlaylist(ss, defaultExpireSeconds)
 
 	// [TODO]: Replace with a minute instead?
 	go func(p *Playlist) {
@@ -136,8 +159,7 @@ func ServePlaylist(rr []string) {
 
 	http.HandleFunc("/library/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
-			// TODO: all responses should be JSON
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			http.Error(w, "{}", http.StatusMethodNotAllowed)
 			return
 		}
 
@@ -151,8 +173,7 @@ func ServePlaylist(rr []string) {
 
 	http.HandleFunc("/play/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
-			// TODO: all responses should be JSON
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			http.Error(w, "{}", http.StatusMethodNotAllowed)
 			return
 		}
 
@@ -161,6 +182,8 @@ func ServePlaylist(rr []string) {
 			Timestamp: time.Now().Unix(),
 			Resource:  resourceName,
 		}
+
+		fmt.Println("Requested to play track:", t)
 
 		err := playlist.Append(t)
 		if err != nil {
@@ -177,8 +200,7 @@ func ServePlaylist(rr []string) {
 
 	http.HandleFunc("/playlist/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
-			// TODO: all responses should be JSON
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			http.Error(w, "{}", http.StatusMethodNotAllowed)
 			return
 		}
 
@@ -189,24 +211,54 @@ func ServePlaylist(rr []string) {
 		fmt.Fprintf(w, "%s", bb)
 	})
 
-	/*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, `<!DOCTYPE html>
 		<html lang="en">
 		<head>
 		<meta charset="utf-8">
-		<title>Gorse</title>
+		<title>Jukebox</title>
 		</head>
-		<body>
+		<body>`)
+
+		fmt.Fprint(w, "<div id=\"sounds\"></div>")
+		fmt.Fprint(w, "<ul>")
+		for _, s := range ss {
+			// TODO: support multiple formats for same name??
+			fmt.Fprint(w, "<li>")
+			ext := path.Ext(s)
+			audioName := strings.TrimSuffix(s, ext)
+			//audioPath := path.Join("/sounds/", s)
+			//fmt.Fprintf(w, "<audio preload=\"auto\" src=\"%s\" id=\"audio_%s\"></audio>", audioPath, s)
+			fmt.Fprintf(w, "<button class=\"play-button\" id=\"button_%s\" data-sound=\"%s\">%s</button>", audioName, s, audioName)
+			fmt.Fprint(w, "</li>")
+		}
+		fmt.Fprint(w, "</ul>")
+		fmt.Fprint(w, `
+		<audio preload="auto" type="audio/mpeg" src="/sounds/56k.mp3" id="hello"></audio>
 		<button type="button" id="mybutton">Push</button>
 		<script type="text/javascript">
 		(function() {
 			"use strict";
 
-			var button = document.getElementById("mybutton");
+			function setupLibrary() {
+				var els = document.getElementsByClassName("play-button");
+				for (var i = 0; i < els.length; i++) {
+					const element = els[i];
+					element.onclick = function() {
+						const snd = element.dataset.sound;
+						fetch('/play/' + snd, {
+							method: "POST",
+						});
+						//document.getElementById('audio_' + snd).play();
+					};
+				}
+			}
+
+			setupLibrary();
+			
+			//var button = document.getElementById("mybutton");
 			window.setInterval(function() {
-				var sec = Date.now() / 1000;
-				console.log("sec = " + sec);
-				fetch('/timestamp')
+				fetch('/playlist/')
 				.catch(function(e) {
 					console.log(e);
 				})
@@ -220,23 +272,36 @@ func ServePlaylist(rr []string) {
 					console.log(e);
 				})
 				.then(function(data) {
-					var pushed = sec < data.timestamp + 2;
-					button.disabled = pushed;
+					const sounds = document.getElementById("sounds");
+					for (var val of data) {
+						var rsrc = val.resource;
+						var audio = sounds.querySelector('[data-sound="' + rsrc + '"]');
+						if (audio == null) {
+							audio = new Audio('/sounds/' + rsrc);
+							sounds.appendChild(audio);
+							audio.dataset.sound = rsrc;
+						}
+						console.log('Playing ' + rsrc);
+						audio.play();
+						
+						//document.getElementById('audio_' + val.resource).play();
+					}
 				});
 			}, 100);
 
-			button.onclick = function() {
+			/*button.onclick = function() {
 				console.log("Pushing the button");
+				document.getElementById('hello').play();
 				// button.disabled = true;
-				fetch("/timestamp", {
+				fetch("/play/56k", {
 					method: "POST",
 				});
-			};
+			};*/
 		})();
 		</script>
 		</body>
 		</html>`)
-	})*/
+	})
 
 	// TODO: set timeouts, max header bytes!
 	// s := &http.Server{
@@ -248,14 +313,39 @@ func ServePlaylist(rr []string) {
 	// }
 	// log.Fatal(s.ListenAndServe())
 
+	fs := http.FileServer(http.Dir("sounds"))
+	http.Handle("/sounds/", http.StripPrefix("/sounds/", fs))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 // time.Now().Unix()
 func main() {
-	validResources := []string{
-		"56k",
-		"deeper",
+	snddir := "sounds"
+	files, err := ioutil.ReadDir(snddir)
+	if err != nil {
+		log.Fatal(err)
 	}
-	ServePlaylist(validResources)
+
+	library := []string{}
+	for _, file := range files {
+		library = append(library, file.Name())
+	}
+
+	// Sort resources alphabetically.
+	sort.Slice(library, func(i, j int) bool {
+		return library[i] < library[j]
+	})
+
+	ServePlaylist(library)
+
+	/*bb, err := ioutil.ReadFile("sounds.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var library map[string]string
+	json.Unmarshal(bb, &library)
+	fmt.Println(library)
+	*/
 }
