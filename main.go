5c19e5c3b26ea1b3d5ffd23bd8433b23cde0803b
Andrew Merenbach <andrew@merenbach.com>
2019-01-01 17:52:59 -0800

    Tweak appearance and don't duplicate playback

--- main.go
+++ main.go
@@ -41,32 +41,37 @@ const defaultExpireSeconds = 5
 // 	Path string `json:"path"`
 // }
 
-// A Track represents a timestamped resource invocation in a playlist.
-type Track struct {
+// A Selection is a request to play something in the library.
+type Selection struct {
 	Resource  string `json:"resource"`
 	Timestamp int64  `json:"timestamp"`
 }
 
+// ID returns a unique identifier for this selection. Algorithm subject to change.
+func (s Selection) ID() string {
+	return fmt.Sprintf("%d%s", s.Timestamp, s.Resource)
+}
+
 // NewerThan determines if this Track is newer than a given number of seconds.
-func (e Track) newerThan(s int64) bool {
-	t := time.Now().Unix()
-	return t-e.Timestamp < s
+func (s Selection) newerThan(ts int64) bool {
+	now := time.Now().Unix()
+	return now-s.Timestamp < ts
 }
 
 // A Playlist contains an ordered list of tracks to play.
 type Playlist struct {
-	Timeout    time.Duration     `json:"timeout"`
-	Library    map[string]string `json:"library"`
-	tracks     []Track
-	tracksLock sync.RWMutex
+	Timeout        time.Duration     `json:"timeout"`
+	Library        map[string]string `json:"library"`
+	selections     []Selection
+	selectionsLock sync.RWMutex
 }
 
 // NewPlaylist creates a new Playlist with the given timeout.
 func NewPlaylist(library map[string]string, s int64) *Playlist {
 	return &Playlist{
-		tracks:  []Track{},
-		Timeout: time.Duration(s),
-		Library: library,
+		selections: []Selection{},
+		Timeout:    time.Duration(s),
+		Library:    library,
 	}
 }
 
@@ -74,43 +79,44 @@ func NewPlaylist(library map[string]string, s int64) *Playlist {
 func (p *Playlist) Prune() {
 	fmt.Println("Pruning...")
 
-	p.tracksLock.Lock()
-	tt := p.tracks[:0]
-	for i, e := range p.tracks {
+	p.selectionsLock.Lock()
+	tt := p.selections[:0]
+	for i, e := range p.selections {
 		if e.newerThan(int64(p.Timeout)) {
-			tt = p.tracks[i:]
+			tt = p.selections[i:]
 			break
 		}
 	}
 	fmt.Println("Pruned to:", tt)
-	p.tracks = tt
+	p.selections = tt
 
-	p.tracksLock.Unlock()
+	p.selectionsLock.Unlock()
 }
 
 // Append a new Track to the end of a Playlist.
-func (p *Playlist) Append(t Track) error {
+func (p *Playlist) Append(t Selection) error {
 	if _, ok := p.Library[t.Resource]; !ok {
 		return errors.New("invalid track")
 	}
 
-	p.tracksLock.Lock()
-	p.tracks = append(p.tracks, t)
-	p.tracksLock.Unlock()
+	p.selectionsLock.Lock()
+	p.selections = append(p.selections, t)
+	p.selectionsLock.Unlock()
 
 	return nil
 }
 
 // Tracks lists tracks in the playlist.
-func (p *Playlist) Tracks() []Track {
-	p.tracksLock.RLock()
-	tt := make([]Track, len(p.tracks))
-	copy(tt, p.tracks)
-	p.tracksLock.RUnlock()
+func (p *Playlist) Tracks() []Selection {
+	p.selectionsLock.RLock()
+	tt := make([]Selection, len(p.selections))
+	copy(tt, p.selections)
+	p.selectionsLock.RUnlock()
 
 	return tt
 }
 
+// Commands lists all available library entries.
 func (p *Playlist) Commands() []string {
 	ss := []string{}
 	for k := range p.Library {
@@ -129,14 +135,14 @@ func (p *Playlist) Commands() []string {
 // Filtered events from the queue.
 func (eq *EventQueue) Filtered() []Event {
 	var out []Event
-	eq.tracksLock.RLock()
+	eq.selectionsLock.RLock()
 	for i, e := range eq.Events {
 		if e.newerThan(int64(eq.Timeout)) {
 			out = eq.Events[i:]
 			break
 		}
 	}
-	eq.tracksLock.RUnlock()
+	eq.selectionsLock.RUnlock()
 	if out == nil {
 		out = []Event{}
 	}
@@ -179,7 +185,7 @@ func ServePlaylist(library map[string]string) {
 		}
 
 		resourceName := path.Base(r.URL.Path)
-		t := Track{
+		t := Selection{
 			Timestamp: time.Now().Unix(),
 			Resource:  resourceName,
 		}
@@ -213,40 +219,61 @@ func ServePlaylist(library map[string]string) {
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//<link href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" rel="stylesheet">
 		fmt.Fprint(w, `<!DOCTYPE html>
 		<html lang="en">
 		<head>
 		<meta charset="utf-8">
 		<title>Jukebox</title>
+		<style>
+			body {
+				background-color: #333;
+				color: #ccc;
+			}
+
+			#selections {
+				padding-left: 0;
+			}
+
+			#selections .selection {
+				display: inline-block;
+				padding: .25em .5em;
+			}
+
+			#selections .play {
+				color: #8f8;
+			}
+		</style>
 		</head>
-		<body>`)
+		<body>
+			<h1>Jukebox</h1>
+			<p>Play sounds for everyone who has this page loaded!</p>
+		`)
 
 		//fmt.Fprint(w, "<div id=\"sounds\"></div>")
-		fmt.Fprint(w, "<ul>")
+		fmt.Fprint(w, "<ul id=\"selections\">")
 		for _, s := range playlist.Commands() {
-			// TODO: support multiple formats for same name??
-			fmt.Fprint(w, "<li>")
-			//audioPath := path.Join("/sounds/", s)
-			fmt.Fprintf(w, "<audio preload=\"auto\" src=\"%s\" id=\"audio_%s\"></audio>", library[s], s)
-			fmt.Fprintf(w, "<button class=\"play-button\" data-sound=\"%s\">%s</button>", s, s)
+			fmt.Fprint(w, "<li class=\"selection\">")
+			fmt.Fprintf(w, "<a class=\"play\" data-sound=\"%s\" href=\"#\">%s</a>", s, s)
+			fmt.Fprintf(w, "<audio preload=\"auto\" src=\"%s\" id=\"audio_%s\" data-timestamp=\"\">Your browser does not support the <code>audio</code> element.</audio>", library[s], s)
 			fmt.Fprint(w, "</li>")
 		}
 		fmt.Fprint(w, "</ul>")
-		fmt.Fprint(w, `<script type="text/javascript">
+		fmt.Fprint(w, `<script>
 		(function() {
 			"use strict";
 
 			function setupLibrary() {
-				var els = document.getElementsByClassName("play-button");
+				var els = document.getElementsByClassName("play");
 				for (var i = 0; i < els.length; i++) {
 					const element = els[i];
-					element.onclick = function() {
+					element.addEventListener("click", function(event) {
+						event.preventDefault();
 						const snd = element.dataset.sound;
 						fetch('/play/' + snd, {
 							method: "POST",
 						});
-						//document.getElementById('audio_' + snd).play();
-					};
+			   		}, false);
 				}
 			}
 
@@ -270,30 +297,24 @@ func ServePlaylist(library map[string]string) {
 				.then(function(data) {
 					//const sounds = document.getElementById("sounds");
 					for (var val of data) {
-						var rsrc = val.resource;
-						var audio = document.getElementById('audio_' + rsrc);
-						/*var audio = sounds.querySelector('[data-sound="' + rsrc + '"]');
-						if (audio == null) {
-							audio = new Audio('/sounds/' + rsrc);
-							sounds.appendChild(audio);
-							audio.dataset.sound = rsrc;
-						}*/
-						console.log('Playing ' + rsrc);
-						audio.play();
-						
-						//document.getElementById('audio_' + val.resource).play();
+						console.log("Evaluating whether to play " + JSON.stringify(val));
+						var audio = document.getElementById('audio_' + val.resource);
+						if (val.timestamp > Number(audio.dataset.timestamp)) {
+							audio.dataset.timestamp = val.timestamp;
+							/*var audio = sounds.querySelector('[data-sound="' + rsrc + '"]');
+							if (audio == null) {
+								audio = new Audio('/sounds/' + rsrc);
+								sounds.appendChild(audio);
+								audio.dataset.sound = rsrc;
+							}*/
+							console.log('Playing ' + val.resource);
+							audio.play();
+						} else {
+							console.log("Already played selection " + val.resource);
+						}
 					}
 				});
 			}, 100);
-
-			/*button.onclick = function() {
-				console.log("Pushing the button");
-				document.getElementById('hello').play();
-				// button.disabled = true;
-				fetch("/play/56k", {
-					method: "POST",
-				});
-			};*/
 		})();
 		</script>
 		</body>
