2f73bfc2b51854effbf620e46c359e272b6ff452
Andrew Merenbach <andrew@merenbach.com>
2019-01-03 21:07:13 -0800

    Trim old code

--- main.go
+++ main.go
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO: support multiple audio formats and don't tightly couple to MP3
 // TODO: emoji responses? handles for participants?
-// TODO: rename Playlist => Library?
 // TODO: replace Track nomenclature
 // TODO: revamp fault tolerance (invalid sound, etc.)
 
@@ -23,151 +21,11 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"path"
-	"sort"
-	"sync"
-	"time"
 )
-
-// Remove from the queue any events older than this.
-// This mitigates potential for endless queue growth.
-const defaultExpireSeconds = 5
-
-// // A Resource represents the details of a resource in the queue.
-// type Resource struct {
-// 	Name string `json:"name"`
-// 	Path string `json:"path"`
-// }
-
-// A StringQueue is a thread-safe queue of strings.
-type StringQueue struct {
-	elements     []string
-	elementsLock sync.RWMutex
-}
-
-// NewStringQueue returns a new StringQueue struct pointer.
-func NewStringQueue() *StringQueue {
-	return &StringQueue{
-		elements: []string{},
-	}
-}
-
-// Elements in the queue.
-func (sq *StringQueue) Elements() []string {
-	sq.elementsLock.RLock()
-	defer sq.elementsLock.RUnlock()
-
-	return sq.elements
-}
-
-// Count the number of elements in the queue.
-func (sq *StringQueue) Count() int {
-	sq.elementsLock.RLock()
-	defer sq.elementsLock.RUnlock()
-
-	return len(sq.elements)
-}
-
-// Push an element on to the end of the queue and return it.
-func (sq *StringQueue) Push(s string) string {
-	sq.elementsLock.Lock()
-	defer sq.elementsLock.Unlock()
-
-	sq.elements = append(sq.elements, s)
-	return s
-}
-
-// ShiftMany pops the given number of elements from the front of the queue and returns them.
-func (sq *StringQueue) ShiftMany(i int) []string {
-	sq.elementsLock.Lock()
-	defer sq.elementsLock.Unlock()
-
-	popped, remaining := sq.elements[:i], sq.elements[i:]
-	sq.elements = remaining
-	return popped
-}
-
-// A BatchStringQueue is a thread-safe queue of strings that
-type BatchStringQueue struct {
-	*StringQueue
-	lastBatchSize      int
-	lastBatchTimestamp int64
-}
-
-// ShiftBatch shifts the last batch count. Repeating the operation twice in a row will empty the queue.
-func (sq *BatchStringQueue) ShiftBatch() []string {
-	els := sq.ShiftMany(sq.lastBatchSize)
-	sq.lastBatchSize = sq.Count()
-	sq.lastBatchTimestamp = time.Now().Unix()
-	return els
-}
-
-// NewBatchStringQueue returns a new StringQueue struct pointer.
-func NewBatchStringQueue() *BatchStringQueue {
-	return &BatchStringQueue{
-		StringQueue: NewStringQueue(),
-	}
-}
-
-// A Playlist contains an ordered list of tracks to play.
-type Playlist struct {
-	TrackLibrary map[string]string `json:"library"`
-	selections   *BatchStringQueue
-}
-
-// NewPlaylist creates a new Playlist with the given timeout.
-func NewPlaylist(library map[string]string, s int64) *Playlist {
-	return &Playlist{
-		selections:   NewBatchStringQueue(),
-		TrackLibrary: library,
-	}
-}
-
-// Prune old items from the event queue.
-func (p *Playlist) Prune() {
-	log.Println("Pruning...")
-	pruned := p.selections.ShiftBatch()
-	log.Println("Pruned the following elements:", pruned)
-}
-
-// Append a new Track to the end of a Playlist.
-func (p *Playlist) Append(t string) string {
-	if _, ok := p.TrackLibrary[t]; !ok {
-		log.Print("invalid track selection:", t)
-		return ""
-	}
-
-	e := p.selections.Push(t)
-	log.Println("Pushed the following element:", e)
-
-	return e
-}
-
-// Selections lists the current queue of track selections from the playlist.
-func (p *Playlist) Selections() (int64, []string) {
-	el := p.selections.Elements()
-	return p.selections.lastBatchTimestamp, el
-}
-
-// Commands lists all available library entries.
-func (p *Playlist) Commands() []string {
-	ss := []string{}
-	for k := range p.TrackLibrary {
-		ss = append(ss, k)
-	}
-
-	// Sort resources alphabetically.
-	sort.Slice(ss, func(i, j int) bool {
-		return ss[i] < ss[j]
-	})
-
-	return ss
-}
 
 /*
 // Filtered events from the queue.
@@ -188,7 +46,7 @@ func (eq *EventQueue) Filtered() []Event {
 }*/
 
 // ServePlaylist runs an HTTP server with a playlist queue.
-func ServePlaylist(library map[string]string) {
+/*func ServePlaylist(library map[string]string) {
 
 	playlist := NewPlaylist(library, defaultExpireSeconds)
 
@@ -281,7 +139,7 @@ func ServePlaylist(library map[string]string) {
 	log.Fatal(http.ListenAndServe(*addr, nil))
 
 	//log.Fatal(http.ListenAndServe(":8080", nil))
-}
+}*/
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
@@ -313,7 +171,7 @@ var addr = flag.String("addr", "localhost:8080", "http service address")
 	}
 }*/
 
-var playlist *Playlist
+var playlistLibrary map[string]string
 
 func main() {
 	bb, err := ioutil.ReadFile("sounds.json")
@@ -321,12 +179,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var resources map[string]string
-	json.Unmarshal(bb, &resources)
-	log.Println("Initializing with the following resources:", resources)
-
-	library := resources
-	playlist = NewPlaylist(library, defaultExpireSeconds)
+	json.Unmarshal(bb, &playlistLibrary)
+	log.Println("Initializing with the following resources:", playlistLibrary)
 
 	flag.Parse()
 	hub := newHub()
@@ -367,38 +221,13 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	data := struct {
-		Url      string
 		Commands map[string]string
 	}{
 		// TODO: allow connecting to arbitrary sound machines!
 		//Url:      "ws://" + r.Host + "/ws",
-		Commands: playlist.TrackLibrary,
+		Commands: playlistLibrary,
 	}
 	homeTemplate.Execute(w, data)
-}
-
-func main2() {
-	bb, err := ioutil.ReadFile("sounds.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var resources map[string]string
-	json.Unmarshal(bb, &resources)
-	log.Println("Initializing with the following resources:", resources)
-
-	/*snddir := "sounds"
-	files, err := ioutil.ReadDir(snddir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	library := []string{}
-	for _, file := range files {
-		library = append(library, file.Name())
-	}*/
-
-	ServePlaylist(resources)
 }
 
 var homeTemplate = template.Must(template.New("").Parse(`<!DOCTYPE html>
@@ -408,7 +237,7 @@ var homeTemplate = template.Must(template.New("").Parse(`<!DOCTYPE html>
 <script type="text/javascript">
 window.onload = function () {
     var conn;
-    var msg = document.getElementById("msg");
+    /*var msg = document.getElementById("msg");
     var log = document.getElementById("log");
 
     function appendLog(item) {
@@ -429,7 +258,7 @@ window.onload = function () {
         conn.send(msg.value);
         msg.value = "";
         return false;
-    };
+    };*/
 
 	
 	////>>
@@ -515,36 +344,9 @@ body {
     background: gray;
 }
 
-#log {
-    background: white;
-    margin: 0;
-    padding: 0.5em 0.5em 0.5em 0.5em;
-    position: absolute;
-    top: 0.5em;
-    left: 0.5em;
-    right: 0.5em;
-    bottom: 3em;
-    overflow: auto;
-}
-
-#form {
-    padding: 0 0.5em 0 0.5em;
-    margin: 0;
-    position: absolute;
-    bottom: 1em;
-    left: 0px;
-    width: 100%;
-    overflow: hidden;
-}*/
-
 </style>
 </head>
 <body>
-<div id="log"></div>
-<form id="form">
-    <input type="submit" value="Send" />
-    <input type="text" id="msg" size="64"/>
-</form>
 <ul id="selections">
 {{range $k, $v := .Commands }}
  <li class="selection">
